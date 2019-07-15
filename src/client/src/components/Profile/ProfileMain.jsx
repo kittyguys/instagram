85f800f404a639a7ff84e24e92644f278ff17a8b
keitoku1993 <keitoku1993@gmail.com>
2019-07-15 15:58:13 +0900

    avatarImage position fix

--- src/client/src/components/Profile/ProfileMain.jsx
+++ src/client/src/components/Profile/ProfileMain.jsx
@@ -3,63 +3,66 @@ import styled from "styled-components";
 import { withRouter } from "react-router";
 
 const ProfileMainWrapper = styled.div`
-	padding-top: 70px;
+  padding-top: 70px;
   display: flex;
-	align-items: center;
+  align-items: center;
 `;
 
 const ProfileAvater = styled.div`
-	width: 70px;
-	height: 70px;
-	border: 2px solid #eee;
-	border-radius: 50%;
-	margin: 0 18px;
+  width: 70px;
+  height: 70px;
+  border: 2px solid #eee;
+  border-radius: 50%;
+  margin: 0 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 `;
 
 const ProfileNameBox = styled.div`
-	display: block;
-	width: 58%;
-`
+  display: block;
+  width: 58%;
+`;
 const ProfileName = styled.div`
-	display: block;
-	font-size: 20px;
-`
+  display: block;
+  font-size: 20px;
+`;
 
 const ProfileEdit = styled.div`
-	display: inline-block;
-	height: 26px;
-	line-height: 26px;
-	text-align: center;
-	border: 1px solid #ddd;
-	margin-top: 10px;
-	text-align: center;
-	width: 100%;
-	border-radius: 4px;
-	cursor: pointer;
-`
+  display: inline-block;
+  height: 26px;
+  line-height: 26px;
+  text-align: center;
+  border: 1px solid #ddd;
+  margin-top: 10px;
+  text-align: center;
+  width: 100%;
+  border-radius: 4px;
+  cursor: pointer;
+`;
 
 const AvatarImage = styled.img`
-	width: 100%;
-`
-
+  width: 100%;
+`;
 
 class ProfileMain extends React.Component {
-    render() {
-		const { id, avater } = this.props;
-        return (
-            <ProfileMainWrapper>
-                <ProfileAvater>
-                    <AvatarImage src={avater} alt="avatar"/>
-                </ProfileAvater>
-                <ProfileNameBox>
-                    <ProfileName>{id}</ProfileName>
-					<ProfileEdit onClick={() => this.props.history.push('/profileedit')}>
-						プロフィールを編集
-					</ProfileEdit>
-                </ProfileNameBox>
-            </ProfileMainWrapper>
+  render() {
+    const { id, avater } = this.props;
+    return (
+      <ProfileMainWrapper>
+        <ProfileAvater>
+          <AvatarImage src={avater} alt="avatar" />
+        </ProfileAvater>
+        <ProfileNameBox>
+          <ProfileName>{id}</ProfileName>
+          <ProfileEdit onClick={() => this.props.history.push("/profileedit")}>
+            プロフィールを編集
+          </ProfileEdit>
+        </ProfileNameBox>
+      </ProfileMainWrapper>
     );
   }
 }
 
-export default withRouter(ProfileMain)
+export default withRouter(ProfileMain);
