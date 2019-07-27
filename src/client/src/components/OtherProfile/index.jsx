18e3e0a3c6db374541be8fa891a8b96231a78f6a
keitoku1993 <keitoku1993@gmail.com>
2019-07-27 17:20:06 +0900

    test

--- src/client/src/components/OtherProfile/index.jsx
+++ src/client/src/components/OtherProfile/index.jsx
@@ -14,7 +14,6 @@ export default class OtherProfile extends React.Component {
   render() {
     return (
       <div>
-        a
         <OtherProfileHeader {...this.props} />
         <OtherProfileMain {...this.props} />
         <OtherProfileStatus {...this.props} />
