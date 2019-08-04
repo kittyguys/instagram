6433664f12bcfeedd20e9278a88e83fb4650e622
keitoku1993 <keitoku1993@gmail.com>
2019-08-04 22:57:36 +0900

    initial commit

--- src/server/db/database.js
+++ src/server/db/database.js
@@ -6,7 +6,9 @@ const database = "instagram";
 class Database {
   connect() {
     mongoose
-      .connect(`mongodb://${server}/${database}`)
+      .connect(
+        `mongodb://heroku_k0cqcb90:2dip978rhsvn4osiul259p9fc4@ds041506.mlab.com:41506/heroku_k0cqcb90`
+      )
       .then(() => {
         console.log("Database connection successful");
       })
